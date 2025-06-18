016ad46e5d6f888e64d04efe3cfaf2f48c6cf644
Utkarsh23072410 <51452641+Utkarsh23072410@users.noreply.github.com>
2025-06-18 20:58:38 +0530

    Market
    Stock

---
+++ Stockmarket.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import yfinance as yf
+import datetime
+ticker_symbol = st.text_input("Name of the stock", "AAPL")
+
+start_date = st.date_input("Start date", datetime.date(2019, 1, 7))
+end_date = st.date_input("End date", datetime.date(2023, 1, 7))
+data =yf.download(ticker_symbol,start=start_date,end=end_date)
+
+
+st.write(data) 
